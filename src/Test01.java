f946cb4ec1d02d24ff086adc541ef5f7d858c2dd
Loverwb <752053325@qq.com>
2019-06-30 00:40:30 +0800

    添加文件

--- src/Test01.java
+++ src/Test01.java
@@ -2,4 +2,8 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("Hello Word");
     }
+
+    public void test(){
+        System.out.println("测试....");
+    }
 }
